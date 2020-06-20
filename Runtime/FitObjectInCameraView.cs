e6c1c009651be5090504185611d9d10854a738c8
Eloi Stree <eloistree@gmail.com>
2020-06-20 05:01:57 +0200

    Save: 06/20/2020 5:01 AM

---
+++ Runtime/FitObjectInCameraView.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitObjectInCameraView : MonoBehaviour
+{
+    public Camera m_cameraTargeted;
+    public Transform m_targetToScale;
+    public float m_multiplicator=1.1f;
+    public float m_additionToNear = 0.001f;
+    public float m_objectDepthSize = 0.1f;
+    public bool m_affectTargetAtStart=true;
+    public bool m_affectTargetAtUpdate;
+    public void Start()
+    {
+        if (m_affectTargetAtStart)
+            ScaleTheObjectToCameraView(m_targetToScale);
+
+    }
+    public void Update()
+    {
+        if (m_affectTargetAtStart)
+            ScaleTheObjectToCameraView(m_targetToScale);
+
+    }
+
+    public  void ScaleTheObjectToCameraView(Transform objectToAffect)
+    {
+        float camNearDist = m_cameraTargeted.nearClipPlane + m_additionToNear;
+        objectToAffect.rotation = m_cameraTargeted.transform.rotation;
+        objectToAffect.position = m_cameraTargeted.transform.position + m_cameraTargeted.transform.forward *( camNearDist+ m_objectDepthSize/2f);
+        Vector3 t, d, l, r;
+        d = m_cameraTargeted.ViewportToWorldPoint(new Vector3(0.5f, 0f, camNearDist));
+        t = m_cameraTargeted.ViewportToWorldPoint(new Vector3(0.5f, 1f, camNearDist));
+        l = m_cameraTargeted.ViewportToWorldPoint(new Vector3(0, 0.5f, camNearDist));
+        r = m_cameraTargeted.ViewportToWorldPoint(new Vector3(1, 0.5f, camNearDist));
+        //Debug.DrawLine(d, t, Color.cyan, 20);
+        //Debug.DrawLine(l, r, Color.blue, 20);
+        objectToAffect.localScale =
+            new Vector3(
+                Vector3.Distance(l, r) * m_multiplicator,
+            Vector3.Distance(t, d) * m_multiplicator,
+            m_objectDepthSize);
+    }
+    private void OnValidate()
+    {
+        if (m_objectDepthSize == 0f)
+            m_objectDepthSize = 0.00001f;
+    }
+}
---
+++ Runtime/HeadPoximityWithObjectsSphereChecker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HeadPoximityWithObjectsSphereChecker : MonoBehaviour
+{
+    public LayerMask m_collisionChecker;
+    public Transform m_castPoint;
+    public float m_minDistance=0.1f;
+    public float m_maxDistance=0.5f;
+    public DistanceChangeEvent m_onDistanceChange;
+    public DistanceChangeEvent m_onIntensityChange;
+    [Header("Debug")]
+    [SerializeField] float m_distanceFound;
+    [SerializeField] float m_intensityFound;
+    [SerializeField] bool m_collisionFound;
+    [SerializeField] Vector3 m_collisionFoundPosition;
+    void Update()
+    {
+        float nearestDistance = 0;
+        CheckCollisionDistance(out m_collisionFound, out nearestDistance, out m_collisionFoundPosition);
+        if (nearestDistance != m_distanceFound)
+        {
+            float intensity = 1f - Mathf.Clamp((nearestDistance - m_minDistance) / (m_maxDistance - m_minDistance), 0f, 1f);
+            m_onIntensityChange.Invoke(intensity);
+            m_onDistanceChange.Invoke(nearestDistance);
+            m_intensityFound = intensity;
+        }
+        else if(!m_collisionFound){
+            m_intensityFound = 0;
+        }
+        m_distanceFound = nearestDistance;
+    }
+
+    private void CheckCollisionDistance(out bool r_foundCollision,out float r_nearestDistance, out Vector3 r_nearestPoint)
+    {
+        r_nearestDistance = float.MaxValue;
+        r_nearestPoint = m_castPoint.position;
+
+        Collider [] colls=  Physics.OverlapSphere(m_castPoint.position, m_maxDistance, m_collisionChecker);
+        r_foundCollision = colls.Length > 0;
+        if (!r_foundCollision) 
+            return;
+        float distanceCollider;
+        Vector3 closestCollider = new Vector3();
+        for (int i = 0; i < colls.Length; i++)
+        {
+            closestCollider = colls[i].ClosestPoint(m_castPoint.position);
+            distanceCollider = Vector3.Distance(closestCollider, m_castPoint.position);
+            if (distanceCollider < r_nearestDistance)
+            {
+                r_nearestDistance = distanceCollider;
+                r_nearestPoint = closestCollider;
+            }
+
+        }
+    }
+
+    [System.Serializable]
+    public class DistanceChangeEvent : UnityEvent<float> { }
+}
